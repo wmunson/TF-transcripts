d76e9eab91622dd2de3a0d2776c6f66c61333b74
Wesley Munson <wmunson@snrs-MacBook-Pro.local>
2018-10-05 09:34:58 -0400

    links list complete

--- src/scraper.py
+++ src/scraper.py
@@ -1,11 +1,26 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 def main():
 	url = 'https://tim.blog/2018/09/20/all-transcripts-from-the-tim-ferriss-show/?utm_source=convertkit&utm_medium=convertkit&utm_campaign=5bf'
 	req = requests.get(url)
 	soup = BeautifulSoup(req.text, 'html.parser')
-	print(soup.content)
+	
+	main = soup.find(id='post-36531')
+	posts = main.findAll('p')
+	hrefs=[]
+	for p in posts:
+		try:
+			href = p.a['href']
+			
+			hrefs.append(href)
+		except:
+			pass
+
+	df = pd.DataFrame(hrefs)
+	df.to_csv('links.csv',index=False)
+	# print(hrefs)
 
 
 if __name__ == '__main__':
